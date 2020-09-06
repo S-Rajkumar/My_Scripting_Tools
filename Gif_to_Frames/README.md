8c3537254a60f0b8d16779cd9816e158a1456e30
udmnxpdu-z <rajkumarsr1903@gmail.com>
2020-09-06 22:52:17 +0530

    update Gif_to_Frames

--- Gif_to_Frames/README.md
+++ Gif_to_Frames/README.md
@@ -5,5 +5,5 @@
 >In CTF's some challenges have `gif` file, there i need to extract frames from the `gif` file that's why i created this tool.
 
 **usage**
->python3 gif_to_frames --input gif_file
+>python3 gif_to_frames.py --input gif_file
 
--- Gif_to_Frames/gif_to_frames.py
+++ Gif_to_Frames/gif_to_frames.py
@@ -7,13 +7,15 @@ Date : July 2020
 
 from PIL import Image
 from PIL import GifImagePlugin
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input','-i',help="Input File",required=True)
 args = parser.parse_args()
 in_file = args.input
-
+dir_name = in_file.split(".")[0]
+os.system("mkdir "+dir_name)
 imageObject = Image.open(in_file)
 
 print("[+] Total Frames : "+str(imageObject.n_frames))
@@ -21,7 +23,7 @@ print("[+] Total Frames : "+str(imageObject.n_frames))
 i = 1; 
 print("[+] Extracting Frames...")
 for frame in range(0,imageObject.n_frames):
-    filename = "frame_"+ str(i) +".png"
+    filename = dir_name+"/frame_"+ str(i) +".png"
     imageObject.seek(frame)
     imageObject.save(filename)
     i = i + 1
